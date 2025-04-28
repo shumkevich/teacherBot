fee8497fe2727f5b4f4716df24b3c652430d1424
shumkevich <169120930+shumkevich@users.noreply.github.com>
2025-04-28 16:04:57 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -122,6 +122,9 @@ def main():
     scheduler.add_job(schedule_daily_notification, CronTrigger(hour=9, minute=0, second=0), args=[application], id="daily_task_check")
     scheduler.start()
 
+ # Сброс вебхука и всех зависших getUpdates
+    application.bot.delete_webhook(drop_pending_updates=True)
+    
     # Запуск бота
     application.run_polling()
 
