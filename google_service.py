011201ac0593ef8ab9efa27e583e59af523be919
shumkevich <169120930+shumkevich@users.noreply.github.com>
2025-04-29 11:04:34 +0500

    google_service.py

--- google_service.py
+++ google_service.py
@@ -1,18 +1,22 @@
 import os
+import requests
 from oauth2client.service_account import ServiceAccountCredentials
 from dotenv import load_dotenv
 
 # Загружаем переменные окружения из .env файла
 load_dotenv()
 
-# Получаем путь к файлу с ключами из переменной окружения
-CREDS_FILE = os.getenv("GOOGLE_CREDS_FILE")
+# Путь для хранения файла с ключами
+CREDS_FILE_PATH = 'apigooglebotdasha.json'
 
-# Проверка, что переменная окружения правильно установлена
-if not CREDS_FILE:
-    raise ValueError("Переменная окружения GOOGLE_CREDS_FILE не установлена или пуста. Проверьте файл .env.")
-else:
-    print(f"Используем файл с ключами: {CREDS_FILE}")
+# Проверка, если файл не существует, скачиваем его
+if not os.path.exists(CREDS_FILE_PATH):
+    print("Ключи Google API не найдены. Скачиваем файл...")
+    url = "https://raw.githubusercontent.com/shumkevich/teacherBot/main/apigooglebotdasha.json"
+    response = requests.get(url)
+    with open(CREDS_FILE_PATH, 'wb') as file:
+        file.write(response.content)
+    print("Файл с ключами успешно скачан.")
 
 # Определение области доступа
 SCOPE = [
@@ -23,7 +27,7 @@ SCOPE = [
 ]
 
 # Подключение к Google Sheets с использованием учетных данных
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, SCOPE)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE_PATH, SCOPE)
 
 # Авторизация
 import gspread
@@ -46,6 +50,7 @@ def update_task_status(row_index, new_status):
 
 
 def update_task_date(row_index, days):
+    from datetime import datetime, timedelta
     new_date = (datetime.now() + timedelta(days=days)).strftime("%Y-%m-%d")
     sheet.update_cell(row_index, 2, new_date)  # Дата — 2-й столбец
     sheet.update_cell(row_index, 3, "отложено")
